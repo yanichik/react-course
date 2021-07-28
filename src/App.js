ba5db60aae5b2c4da573800dfbb24ea9c8f2f365
yanichik <brunshte@gmail.com>
2021-07-27 17:27:36 -0700

    reset app to original code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,4 @@
-import Todo from "./components/Todo";
-function App(props) {
-	function remove(txt) {
-		console.log("Removing " + txt);
-	}
-	return (
-		<div>
-			<h1>My TODOS</h1>
-			<Todo text="First" removeItem={remove} />
-			<Todo text="Second" removeItem={remove} />
-			<Todo text="Third" removeItem={remove} />
-		</div>
-	);
+function App() {
+	return <div></div>;
 }
 export default App;
