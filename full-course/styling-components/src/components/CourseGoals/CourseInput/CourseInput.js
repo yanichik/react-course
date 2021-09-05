10ef1d47fff8273dcff2ac44f666b292a5edbfb3
yanichik <brunshte@gmail.com>
2021-09-05 15:10:14 -0700

    convert styling to css modules

--- full-course/styling-components/src/components/CourseGoals/CourseInput/CourseInput.js
+++ full-course/styling-components/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,38 +1,38 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 import Button from "../../UI/Button/Button";
-// import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 
 // div as a styled component
-const FormControl = styled.div`
-	margin: 0.5rem 0;
+// const FormControl = styled.div`
+// 	margin: 0.5rem 0;
 
-	& label {
-		font-weight: bold;
-		display: block;
-		margin-bottom: 0.5rem;
-    // dynamic color inside the styled component
-    color: ${props => props.invalid ? 'red' : 'black'};
-	}
+// 	& label {
+// 		font-weight: bold;
+// 		display: block;
+// 		margin-bottom: 0.5rem;
+//     // dynamic color inside the styled component
+//     color: ${props => props.invalid ? 'red' : 'black'};
+// 	}
 
-	& input {
-		display: block;
-		width: 100%;
-		border: 1px solid #ccc;
-		font: inherit;
-		line-height: 1.5rem;
-		padding: 0 0.25rem;
-    background-color: ${props => props.invalid ? 'salmon' : 'transparent'};
-		border-color: ${props => props.invalid ? 'red' : 'black'};
-	}
+// 	& input {
+// 		display: block;
+// 		width: 100%;
+// 		border: 1px solid #ccc;
+// 		font: inherit;
+// 		line-height: 1.5rem;
+// 		padding: 0 0.25rem;
+//     background-color: ${props => props.invalid ? 'salmon' : 'transparent'};
+// 		border-color: ${props => props.invalid ? 'red' : 'black'};
+// 	}
 
-	& input:focus {
-		outline: none;
-		background: #fad0ec;
-		border-color: #8b005d;
-	}
+// 	& input:focus {
+// 		outline: none;
+// 		background: #fad0ec;
+// 		border-color: #8b005d;
+// 	}
 
-`;
+// `;
 
 const CourseInput = (props) => {
 	const [enteredValue, setEnteredValue] = useState("");
@@ -59,10 +59,14 @@ const CourseInput = (props) => {
 		<form onSubmit={formSubmitHandler}>
 			{/* add logic to dynamically include 'invalid' class when isValid is false */}
 			{/* FormControl is a div styled component */}
-			<FormControl invalid={!isValid}>
+			{/* <FormControl invalid={!isValid}> */}
+			<div
+				className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+			>
 				<label>Course Goal</label>
 				<input type="text" onChange={goalInputChangeHandler} />
-			</FormControl>
+			</div>
+			{/* </FormControl> */}
 			<Button type="submit">Add Goal</Button>
 		</form>
 	);
--- full-course/styling-components/src/components/UI/Button/Button.js
+++ full-course/styling-components/src/components/UI/Button/Button.js
@@ -1,33 +1,39 @@
-import styled from "styled-components";
+// import styled from "styled-components";
+import styles from "./Button.module.css";
 
-const Button = styled.button.attrs({
-	type: "submit",
-})`
-	width: 100%;
-	font: inherit;
-	padding: 0.5rem 1.5rem;
-	border: 1px solid #8b005d;
-	color: white;
-	background: #8b005d;
-	box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-	cursor: pointer;
-
-	@media (min-width: 768px) {
-		width: auto;
-	}
-
-	$:focus {
-		outline: none;
-	}
-
-	$:hover,
-	$:active {
-		background: #ac0e77;
-		border-color: #ac0e77;
-		box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-	}
-
-	${(props) => console.log(props)}
-`;
+function Button(props) {
+	return <button className={styles.button}>{props.children}</button>;
+}
 
 export default Button;
+
+// Styled Components Method
+// const Button = styled.button.attrs({
+// 	type: "submit",
+// })`
+// 	width: 100%;
+// 	font: inherit;
+// 	padding: 0.5rem 1.5rem;
+// 	border: 1px solid #8b005d;
+// 	color: white;
+// 	background: #8b005d;
+// 	box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+// 	cursor: pointer;
+
+// 	@media (min-width: 768px) {
+// 		width: auto;
+// 	}
+
+// 	$:focus {
+// 		outline: none;
+// 	}
+
+// 	$:hover,
+// 	$:active {
+// 		background: #ac0e77;
+// 		border-color: #ac0e77;
+// 		box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+// 	}
+
+// 	${(props) => console.log(props)}
+// `;
