9ba1d128572b0b435cb3a756c7fec9e4a8df8b67
yanichik <brunshte@gmail.com>
2021-09-22 22:48:23 -0700

    use portal to place modal in higher level on the document node

--- full-course/add-user-v2/src/components/UI/ErrorModal.js
+++ full-course/add-user-v2/src/components/UI/ErrorModal.js
@@ -1,19 +1,43 @@
+import ReactDOM from "react-dom";
 import styles from "./ErrorModal.module.css";
 import Card from "./Card";
 import Button from "./Button";
+
+function Backdrop(props) {
+	return <div className={styles.backdrop} onClick={props.onConfirm} />;
+}
+
+function Overlay(props) {
+	return (
+		<Card className={styles.modal}>
+			<header className={styles.header}>
+				<h2>{props.title}</h2>
+			</header>
+			<div className={styles.content}>{props.message}</div>
+			<footer className={styles.actions}>
+				<Button type="submit" onClick={props.onConfirm}>
+					Okay
+				</Button>
+			</footer>
+		</Card>
+	);
+}
+
 function ErrorModal(props) {
 	return (
 		<>
-			<div className={styles.backdrop} onClick={props.onConfirm} />
-			<Card className={styles.modal}>
-				<header className={styles.header}>
-					<h2>{props.title}</h2>
-				</header>
-				<div className={styles.content}>{props.message}</div>
-				<footer className={styles.actions}>
-					<Button type="submit" onClick={props.onConfirm}>Okay</Button>
-				</footer>
-			</Card>
+			{ReactDOM.createPortal(
+				<Backdrop onConfirm={props.onConfirm} />,
+				document.getElementById("root-backdrop")
+			)}
+			{ReactDOM.createPortal(
+				<Overlay
+					title={props.title}
+					message={props.message}
+					onConfirm={props.onConfirm}
+				/>,
+				document.getElementById("root-overlay")
+			)}
 		</>
 	);
 }
