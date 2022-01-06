0efc468c0874ca3236c7cb7f7b41909b4852e34a
yanichik <brunshte@gmail.com>
2022-01-05 22:20:39 -0800

    update

--- full-course/forms/src/components/SimpleInput.js
+++ full-course/forms/src/components/SimpleInput.js
@@ -2,15 +2,21 @@ import { useState } from "react";
 
 const SimpleInput = (props) => {
 	const [enteredValue, setEnteredValue] = useState("");
+	const [enteredEmail, setEnteredEmail] = useState("");
 	const [enteredValueTouched, setEnteredValueTouched] = useState(false);
+	const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+	let formIsValid = false;
 
 	// instead of using state to track entered value validity, using the
 	// enteredValue state to DERIVE validity. This CAN BE done b/c the component
 	// re-renders with each change in enteredValue state, thus CAN rely
 	// on this other state and derive validity
 	const enteredValueValid = enteredValue.trim() !== "";
-
-	const inputChangeHandler = (event) => {
+	const enteredEmailValid = enteredEmail.includes("@");
+	if (enteredValueValid && enteredEmailValid) {
+		formIsValid = true;
+	}
+	const nameChangeHandler = (event) => {
 		setEnteredValue(event.target.value);
 		// using event.target.value BECAUSE if checking the enteredValue, you are ACTUALLY
 		// checking an OLD state b/c the setState function ONLY schedules the change -
@@ -19,41 +25,68 @@ const SimpleInput = (props) => {
 		// 	setEnteredValueValid(true);
 		// }
 	};
+	const emailChangeHandler = (event) => {
+		setEnteredEmail(event.target.value);
+	};
+
 	const submitHandler = (event) => {
 		event.preventDefault();
 		setEnteredValueTouched(true);
-		if (enteredValue.trim() === "") {
-			return;
-		}
+		setEnteredEmailTouched(true);
+		// 	if (enteredValue.trim() === "") {
+		// 		return;
+		// 	}
 		setEnteredValue("");
+		setEnteredEmail("");
 		setEnteredValueTouched(false);
-		// inputRef.current.focus();
+		setEnteredEmailTouched(false);
+		// 	// inputRef.current.focus();
 	};
-	const inputBlurHandler = (event) => {
+	const nameBlurHandler = (event) => {
 		setEnteredValueTouched(true);
 	};
-	const nameInputClasses =
-		enteredValueTouched && !enteredValueValid
-			? "form-control invalid"
-			: "form-control";
+	const emailBlurHandler = (event) => {
+		setEnteredEmailTouched(true);
+	};
+
+	const inputClassesFunction = (touched, valid) => {
+		return touched && !valid ? "form-control invalid" : "form-control";
+	};
+  
 	return (
 		<form onSubmit={submitHandler}>
-			<div className={nameInputClasses}>
-				{/* <div className='form-control'> */}
+			<div
+				className={inputClassesFunction(enteredValueTouched, enteredValueValid)}
+			>
 				<label htmlFor="name">Your Name</label>
 				<input
 					type="text"
 					id="name"
-					onChange={inputChangeHandler}
-					onBlur={inputBlurHandler}
+					onChange={nameChangeHandler}
+					onBlur={nameBlurHandler}
 					value={enteredValue}
 				/>
 				{enteredValueTouched && !enteredValueValid && (
 					<p className={"error-text"}>Invalid Entry</p>
 				)}
 			</div>
+			<div
+				className={inputClassesFunction(enteredEmailTouched, enteredEmailValid)}
+			>
+				<label htmlFor="email">Email</label>
+				<input
+					type="email"
+					id="email"
+					onChange={emailChangeHandler}
+					onBlur={emailBlurHandler}
+					value={enteredEmail}
+				/>
+				{enteredEmailTouched && !enteredEmailValid && (
+					<p className={"error-text"}>Invalid Entry</p>
+				)}
+			</div>
 			<div className="form-actions">
-				<button>Submit</button>
+				<button disabled={!formIsValid}>Submit</button>
 			</div>
 		</form>
 	);
