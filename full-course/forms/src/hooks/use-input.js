476ce94de22e89d2f5e9906e21d94905ac2ea1cc
yanichik <brunshte@gmail.com>
2022-01-07 08:44:23 -0800

    complete simpleInput

--- full-course/forms/src/hooks/use-input.js
+++ full-course/forms/src/hooks/use-input.js
@@ -1,30 +1,51 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInput1State = {
+	value: "",
+	isTouched: false,
+};
+
+const input1Reducer = (state, action) => {
+	if (action.type === "INPUT") {
+		// MUST return all values within state object, otherwise changing object params
+		return { value: action.value, isTouched: state.isTouched };
+	}
+	if (action.type === "BLUR") {
+		return { value: state.value, isTouched: true };
+	}
+	if (action.type === "RESET") {
+		return { value: "", isTouched: false };
+	}
+	return initialInput1State;
+};
 
 // pass in validateVal function instead of defining inside the useInput hook
 // BECAUSE this allows to generalize the validity function
 const useInput = (validateVal) => {
-	const [input1, setInput1] = useState("");
-	const [touchedInput1, setTouchedInput1] = useState(false);
+	// initializes reduer according the the initial input state (initialInput1State)
+	// & reducer logic (input1Reducer function).
+	const [input1State, dispatch] = useReducer(input1Reducer, initialInput1State);
 
-	const input1Valid = validateVal(input1);
-	const input1HasError = !input1Valid && touchedInput1;
+	const input1Valid = validateVal(input1State.value);
+	const input1HasError = !input1Valid && input1State.isTouched;
 
 	const input1ChangeHandler = (event) => {
-		setInput1(event.target.value);
+		dispatch({ type: "INPUT", value: event.target.value });
+		// setInput1(event.target.value);
 	};
 
 	const input1BlurHandler = () => {
-		setTouchedInput1(true);
+		dispatch({ type: "BLUR" });
+		// setTouchedInput1(true);
 	};
 
 	const resetInput1 = () => {
-		setInput1("");
-		setTouchedInput1(false);
+		dispatch({ type: "RESET" });
 	};
 
 	return {
-		value: input1,
-		isTouched: touchedInput1,
+		value: input1State.value,
+		isTouched: input1State.isTouched,
 		isValid: input1Valid,
 		changeHandler: input1ChangeHandler,
 		hasError: input1HasError,
