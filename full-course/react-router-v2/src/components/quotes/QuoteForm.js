4d8e17438b66ce67a03fafef0673e5c73cd92c8f
yanichik <brunshte@gmail.com>
2022-01-23 08:31:24 -0800

    use <Prompt /> to prevent unwanted navigation away from form

--- full-course/react-router-v2/src/components/quotes/QuoteForm.js
+++ full-course/react-router-v2/src/components/quotes/QuoteForm.js
@@ -1,4 +1,5 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { Prompt } from "react-router-dom";
 
 import Card from "../UI/Card";
 import LoadingSpinner from "../UI/LoadingSpinner";
@@ -7,6 +8,7 @@ import classes from "./QuoteForm.module.css";
 const QuoteForm = (props) => {
 	const authorInputRef = useRef();
 	const textInputRef = useRef();
+	const [isFormStarted, setIsFormStarted] = useState(false);
 
 	function submitFormHandler(event) {
 		event.preventDefault();
@@ -17,31 +19,58 @@ const QuoteForm = (props) => {
 		// optional: Could validate here
 
 		props.onAddQuote({ author: enteredAuthor, text: enteredText });
+	}
 
+	function properSubmitHandler() {
+		setIsFormStarted(false);
+	}
+
+	function startedFormHandler() {
+		setIsFormStarted(true);
 	}
 
 	return (
-		<Card>
-			<form className={classes.form} onSubmit={submitFormHandler}>
-				{props.isLoading && (
-					<div className={classes.loading}>
-						<LoadingSpinner />
-					</div>
-				)}
+		<>
+			<Prompt
+				when={isFormStarted}
+				message={(location) =>
+					"All info will be lost if you leave. Do you want to continue?"
+				}
+			/>
+			<Card>
+				<form className={classes.form} onSubmit={submitFormHandler}>
+					{props.isLoading && (
+						<div className={classes.loading}>
+							<LoadingSpinner />
+						</div>
+					)}
 
-				<div className={classes.control}>
-					<label htmlFor="author">Author</label>
-					<input type="text" id="author" ref={authorInputRef} />
-				</div>
-				<div className={classes.control}>
-					<label htmlFor="text">Text</label>
-					<textarea id="text" rows="5" ref={textInputRef}></textarea>
-				</div>
-				<div className={classes.actions}>
-					<button className="btn">Add Quote</button>
-				</div>
-			</form>
-		</Card>
+					<div className={classes.control}>
+						<label htmlFor="author">Author</label>
+						<input
+							onChange={startedFormHandler}
+							type="text"
+							id="author"
+							ref={authorInputRef}
+						/>
+					</div>
+					<div className={classes.control}>
+						<label htmlFor="text">Text</label>
+						<textarea
+							onChange={startedFormHandler}
+							id="text"
+							rows="5"
+							ref={textInputRef}
+						></textarea>
+					</div>
+					<div className={classes.actions}>
+						<button onClick={properSubmitHandler} className="btn">
+							Add Quote
+						</button>
+					</div>
+				</form>
+			</Card>
+		</>
 	);
 };
 
