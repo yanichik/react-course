896b3719196694d5c4257aa39ff4d4160303873d
yanichik <brunshte@gmail.com>
2022-02-07 22:41:17 -0800

    add pw change - very simple without validation nor confirmation

--- full-course/auth/src/components/Profile/ProfileForm.js
+++ full-course/auth/src/components/Profile/ProfileForm.js
@@ -1,17 +1,37 @@
-import classes from './ProfileForm.module.css';
+import classes from "./ProfileForm.module.css";
+import { useRef, useContext } from "react";
+import { AuthContext } from "../../store/auth-context";
 
 const ProfileForm = () => {
-  return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
-  );
-}
+	const authCtx = useContext(AuthContext);
+	const newPasswordRef = useRef();
+	const submitHandler = (event) => {
+		event.preventDefault();
+		const enteredPassword = newPasswordRef.current.value;
+		fetch(
+			"https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAg7hKEJZQz6TCgVGOqbcLcmnZOwLBsyuc",
+			{
+				method: "POST",
+				body: JSON.stringify({
+					idToken: authCtx.token,
+					password: enteredPassword,
+					returnSecureToken: false,
+				}),
+				headers: { "Content-type": "application/json" },
+			}
+		).then((res) => {});
+	};
+	return (
+		<form className={classes.form} onSubmit={submitHandler}>
+			<div className={classes.control}>
+				<label htmlFor="new-password">New Password</label>
+				<input type="password" id="new-password" ref={newPasswordRef} />
+			</div>
+			<div className={classes.action}>
+				<button>Change Password</button>
+			</div>
+		</form>
+	);
+};
 
 export default ProfileForm;
